e0fbe553a10272677c76cc05d57d1966a9fbdcb3
Manik <guptamanik1921@gmail.com>
2020-06-24 20:26:37 +0530

    update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Post
 
 
 def home(request):
-    
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+def home1(request):
+    return render(request, 'blog/home1.html')
