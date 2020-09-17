caadb2af0677388284bdb52b02b31b8825a0a3cf
Manik <guptamanik1921@gmail.com>
2020-09-17 16:55:25 +0530

    update

--- internshipPortal/models.py
+++ internshipPortal/models.py
@@ -28,6 +28,18 @@ class Internship(models.Model):
     def about_markdown(self):
         about = self.about
         return mark_safe(markdown(about))
+    
+    def get_next_post(self):
+        next_post = self.get_previous_by_apply_by()
+        if next_post:
+        	return next_post
+        return False   	
+
+    def get_prev_post(self):
+        prev_post = self.get_next_by_apply_by()
+        if prev_post:
+        	return prev_post
+        return False
 
 class InternshipApplication(models.Model):
     internship = models.ForeignKey(Internship, on_delete=models.CASCADE, default='', related_name='internship')
